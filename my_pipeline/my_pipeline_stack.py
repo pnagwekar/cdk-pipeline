eac1e7be5be450868cff6b5163c81c6b15eb533c
Parag Nagwekar <pnagweka@amazon.com>
2023-09-21 17:10:53 +0000

    Context adding

--- my_pipeline/my_pipeline_stack.py
+++ my_pipeline/my_pipeline_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
-from my_pipeline.my_pipeline_app_stage import MyPipelineAppStage
+from my_pipeline.my_pipeline_app_dev_stage import MyPipelineAppDevStage
 from aws_cdk.pipelines import ManualApprovalStep
 
 class MyPipelineStack(cdk.Stack):
@@ -9,17 +9,22 @@ class MyPipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        repoName = self.node.try_get_context("repoName")
+        dev_account = self.node.try_get_context("dev.account")
+        dev_region = self.node.try_get_context("dev.region")
+
         pipeline =  CodePipeline(self, "Pipeline",
                         pipeline_name="CDKPipeline",
                         synth=ShellStep("Synth",
-                            input=CodePipelineSource.git_hub("pnagwekar/cdk-pipeline", "main"),
+                            input=CodePipelineSource.git_hub(repoName, "main"),
                             commands=["npm install -g aws-cdk",
                                 "python -m pip install -r requirements.txt",
                                 "cdk synth"]
                         )
                     )
 
-        testing_stage = pipeline.add_stage(MyPipelineAppStage(self, "infrastructure-Deploy",
-            env=cdk.Environment(account="299344511603", region="us-east-1")))
+        testing_stage = pipeline.add_stage(MyPipelineAppDevStage(self, "infrastructure-Deploy",
+            env=cdk.Environment(account=dev_account, region=dev_region)))
 
         testing_stage.add_post(ManualApprovalStep('approval'))
+
