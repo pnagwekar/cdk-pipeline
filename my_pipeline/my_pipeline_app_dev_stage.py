815f4c7f19fe253cf98eed4ca727622049f565fa
Parag Nagwekar <pnagweka@amazon.com>
2023-09-21 17:16:41 +0000

    Context adding

--- my_pipeline/my_pipeline_app_dev_stage.py
+++ my_pipeline/my_pipeline_app_dev_stage.py
@@ -3,7 +3,7 @@ from constructs import Construct
 from my_pipeline.my_pipeline_lambda_stack import MyLambdaStack
 from my_pipeline.my_pipeline_s3_stack import S3Stack
 
-class MyPipelineAppStage(cdk.Stage):
+class MyPipelineAppDevStage(cdk.Stage):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
