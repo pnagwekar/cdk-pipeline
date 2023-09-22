d35b8c5a8ae5273e7f7769d78b979d0593df4145
Parag Nagwekar <pnagweka@amazon.com>
2023-09-22 06:13:28 +0000

    README.MD

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ MyPipelineStack(app, "MyPipelineStack",
     #env=cdk.Environment(account='123456789012', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    env=cdk.Environment(account=dev_info["account], region=dev_info["region"]
+    env=cdk.Environment(account=dev_info["account"], region=dev_info["region"]
     )
 )
 
